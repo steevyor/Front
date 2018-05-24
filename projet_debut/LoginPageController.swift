63e4219da181065faafc663b38a6e20f143c5b0b
m2sar <m2sar@ppti-14-409-08.ufr-info-p6.jussieu.fr>
2018-05-24 11:13:40 +0200

    getFriend et getToken

--- projet_debut/LoginPageController.swift
+++ projet_debut/LoginPageController.swift
@@ -31,60 +31,108 @@ class LoginPageController: UIViewController
     @IBAction func connexion(_ sender: Any)
     
     {
-
-        //print("Bouton de connexion appuyé")
-        
-        let login = mail.text
-        let password = mdp.text
+        let login = self.mail.text
+        let password = self.mdp.text
         
         if (login?.isEmpty)! || (password?.isEmpty)!
         {
             let monAlerte = UIAlertController(title: "☔️", message: "Veuillez renseigner tous les champs", preferredStyle: UIAlertControllerStyle.alert)
             monAlerte.addAction(UIAlertAction(title: "Annuler", style: UIAlertActionStyle.default,handler: nil))
             self.present(monAlerte, animated: true, completion: nil)
+
             
-            return
         }
         else
         {
             return
-            let parameters = [
-                "login": "\(login!)",
-                "login": "\(password!)"
-            ]
+                connexion(login: login! ,password: password!)
+
             
-            UIApplication.shared.isNetworkActivityIndicatorVisible = true
-            
-            let feedURL = "163.172.154.4:8080/dant/api/test/authentificate"
-            var request = URLRequest(url: URL(string: feedURL)!)
-            
-            request.httpMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            
-            guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
-            request.httpBody = httpBody
-            _ = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                if let jsonData = data ,
-                    let feed = (try? JSONSerialization.jsonObject(with: jsonData , options: .mutableContainers)) as? NSDictionary , let token = feed.value(forKeyPath: "feed.entry.im:token.label") as? String
-                {
-                    
-                    let saveSuccessful: Bool = KeychainWrapper.standard.set(token, forKey: "myKey")
-                    print("\(saveSuccessful)")
-                }
-                    //let artist = feed.value(forKeyPath: "feed.entry.im:artist.label") as? String ,
-                    //let imageURLs = feed.value(forKeyPath: "feed.entry.im:image") as? [NSDictionary] {
-                    //if let imageURL = imageURLs.last ,
-                        /*let imageURLString = imageURL.value(forKeyPath: "label") as? String{
-                        self.loadImage(from: URL(string:imageURLString)!)
-                        self.titleLabel.text = title
-                        self.titleLabel.isHidden = false
-                        self.arstistLabel.text = artist
-                        self.arstistLabel.isHidden = false
-                        */
-                }.resume()
             
         }
     }
+    
+    
+    
+    func connexion(login: String, password: String){
+        let parameters = [
+            "login": "\(login)",
+            "login": "\(password)"
+        ]
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        let feedURL = "163.172.154.4:8080/dant/api/test/authentificate"
+        var request = URLRequest(url: URL(string: feedURL)!)
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
+        request.httpBody = httpBody
+        _ = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let jsonData = data ,
+                let feed = (try? JSONSerialization.jsonObject(with: jsonData , options: .mutableContainers)) as? NSDictionary , let token = feed.value(forKeyPath: "feed.entry.im:token.label") as? String
+            {
+                
+                let saveSuccessful: Bool = KeychainWrapper.standard.set(token, forKey: "myKey")
+                print("\(saveSuccessful)")
+            }
+            //let artist = feed.value(forKeyPath: "feed.entry.im:artist.label") as? String ,
+            //let imageURLs = feed.value(forKeyPath: "feed.entry.im:image") as? [NSDictionary] {
+            //if let imageURL = imageURLs.last ,
+            /*let imageURLString = imageURL.value(forKeyPath: "label") as? String{
+             self.loadImage(from: URL(string:imageURLString)!)
+             self.titleLabel.text = title
+             self.titleLabel.isHidden = false
+             self.arstistLabel.text = artist
+             self.arstistLabel.isHidden = false
+             */
+            }.resume()
+        getFriends()
+        getToken()
+
+        
+    }
+    
+    func getToken(){
+        let feedURL = "163.172.154.4:8080/dant/api/test/token"    // verifier adresse
+        var request = URLRequest(url: URL(string: feedURL)!)
+        let session = URLSession.shared.dataTask(with: request ,completionHandler:
+        { (data, response, error) in
+            if let jsonData = data ,
+                let feed = (try? JSONSerialization.jsonObject(with: jsonData , options: .mutableContainers)) as? NSDictionary ,
+                let token = feed.value(forKeyPath: "feed.entry.im:toke,.label") as? String {
+                self.token = token
+            }
+        })
+        
+        session.resume()
+        
+    }
+    
+    func getFriends()
+    {
+        let feedURL = "163.172.154.4:8080/dant/api/test/position"    // verifier adresse
+        var request = URLRequest(url: URL(string: feedURL)!)
+        let session = URLSession.shared.dataTask(with: request ,completionHandler:
+        { (data, response, error) in
+            if let jsonData = data ,
+                let feed = (try? JSONSerialization.jsonObject(with: jsonData , options: .mutableContainers)) as? NSDictionary ,
+                let login = feed.value(forKeyPath: "feed.entry.im:login.label") as? String ,
+                let longitude = feed.value(forKeyPath: "feed.entry.im:longitude.label") as? String ,
+                let latitude = feed.value(forKeyPath: "feed.entry.im:latitude.label") as? String {
+                var f: Friend = Friend.init(pseudo: login)
+                f.setCoordinates(latitude: longitude as! CLLocationDegrees, longitude: latitude as! CLLocationDegrees)
+            }
+        })
+        
+        //créer un tableau de friends pour l'afficher
+        session.resume()
+    }
+    
+    
+    
     
     //Envoyer à la vue suivante les amis récupérés et le token
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
